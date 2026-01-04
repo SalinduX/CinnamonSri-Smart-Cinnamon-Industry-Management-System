111ed7104b2e151a6681c2938b100bf22560cc69
SalinduX <it22180834@sliit.com>
2026-01-04 11:06:58 +0530

    update-clean the code

---
+++ CinnamonSriApp/app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+
+
+const index = () => {
+  return (
+    <View>
+      <Text
+        style={{
+            color: 'red',
+            fontSize: 20,
+        }}>index</Text>
+    </View>
+  )
+}
+
+export default index
